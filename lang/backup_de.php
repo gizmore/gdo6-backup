2342bb834181fb67ec5f31377380c68310e8a650
gizmore <gizmore@wechall.net>
2019-08-31 16:24:58 +0200

    6.10-r42_german_lang

---
+++ lang/backup_de.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+### Config
+'cfg_backup_lastdate' => 'Datum des letzten Backups',
+'cfg_backup_send_mail' => 'Backups per Email senden?',
+'cfg_tt_backup_send_mail' => 'Das Backup ist vielleicht zu groß :(',
+### Mail
+'mail_subj_backup' => '%s: Backup',
+'' => '
+Hallo %s,
+
+Das neueste Backup ist angehängt.
+
+Viele Grüße,
+Das %s System.',
+);
